46b7585c8f7c030995ded7ed496eb0e024ff16b9
MassacesiJuan03 <111523496+MassacesiJuan03@users.noreply.github.com>
2023-04-01 01:07:18 -0300

    Add files via upload

--- practicas/tp-complejidad/code/main.py
+++ practicas/tp-complejidad/code/main.py
@@ -1,5 +1,5 @@
-from linkedlist import insert, length
 from math import trunc
+from random import randint
 
 class LinkedList:
   head = None
@@ -12,8 +12,8 @@ def Contiene_Suma(A,n):
     if A is None:
         return
     else:
-        for i in range(len(A)):
-            for j in range(i, len(A)):
+        for i in range(len(A)-1):
+            for j in range(i+1, len(A)):
                 if A[i] + A[j] == n:
                     return True
     return
@@ -25,125 +25,87 @@ if output is True:
     print("Existe el par")
 else:
     print("No existe un par")  
-"""-----------------------------------------------------------"""
-List = LinkedList()
-insert(List, 7, 0)
-insert(List, 3, 1)
-insert(List, 2, 2)
-insert(List, 8, 3)
-insert(List, 5, 4)
-insert(List, 4, 5)
-insert(List, 1, 6)
-insert(List, 6, 7)
-insert(List, 10, 8)
-insert(List, 9, 9)
-def printList(L):
-    currentNode = L.head
-    print("[", end="")
-    cont = 0 
-    while currentNode != None:
-        if cont == length(L)-1:
-            print(currentNode.value, end="")
-        else:
-            print(currentNode.value, end=" ")
-        cont += 1
-        currentNode = currentNode.nextNode
-    print("]")
 
-print("Lista Base:")
-printList(List)
-print("")
 """----------------------------------------------------"""
-
-def MergeSort(L):
-    LeftList = LinkedList()
-    RightList = LinkedList()
-    if length(L) == 1:
-        return L
-    
-    if length(L) > 1:
-        currentNode = L.head
-        long = trunc((length(L)/2))
+#Ejercicio 4
+def deleteByPosition(list, position, value):
+    for i in range(len(list)):
+        if i == position:
+            list.remove(value)
+            
+def search(list, value, positionPivot):
+    for i in range(positionPivot, len(list)):
+        if list[i] == value:
+            return i   
         
-        #Find the value of pivot
-        auxCurrentNode = L.head
-        positionPivot = 0
-        if (length(L)/2) == long:
-            while auxCurrentNode != None:
-                if positionPivot == long-1:
-                    pivot = auxCurrentNode.value
-                    break
-                auxCurrentNode = auxCurrentNode.nextNode
-                positionPivot += 1
-        else:
-            while auxCurrentNode != None:
-                if positionPivot == long:
-                    pivot = auxCurrentNode.value
-                    break
-                auxCurrentNode = auxCurrentNode.nextNode
-                positionPivot += 1
+longitud = int(input("Ingresar la longitud de la lista: "))
+list = []
+for i in range(longitud):
+    list.append(randint(0,10))
+print(list)
+
+listMinLeft = []
+listMinRight = []
+
+if len(list)/2 == trunc(len(list)/2):
+    #LISTA DE LONGITUD PAR
+    positionPivote = trunc(len(list)/2) - 1
+    pivote = list[positionPivote]
+    for i in range(len(list)):
+        if list[i] < pivote:
+            if i < positionPivote:
+                listMinLeft.append(list[i])
+            else:
+                listMinRight.append(list[i])
+else:
+    #LISTA DE LONGITUD IMPAR
+    positionPivote = trunc(len(list)/2)
+    pivote = list[positionPivote]
+    for i in range(len(list)):
+        if list[i] < pivote:
+            if i < positionPivote:
+                listMinLeft.append(list[i])
+            else:
+                listMinRight.append(list[i]) 
+
+print(listMinLeft)
+print(listMinRight)
+print("----------------------------")
+contMaxLeft = 0
+contMaxRight = positionPivote
+while abs(len(listMinLeft) - len(listMinRight)) > 1:
+    if len(listMinLeft) > len(listMinRight):
+        if contMaxRight < len(list):
+            if list[contMaxRight] >= pivote:
+                MaxRight = list[contMaxRight]
+                del list[contMaxRight]
+                position = search(list, listMinLeft[0], 0)
+                deleteByPosition(list, position, listMinLeft[0])
+                list.insert(0,MaxRight)
+                list.append(listMinLeft[0])
+                value = listMinLeft[0]
+                listMinLeft.remove(value)
+                listMinRight.append(value)
+        contMaxLeft += 1         
+    else:
+        if contMaxLeft < positionPivote:
+            if list[contMaxLeft] >= pivote:
+                MaxLeft = list[contMaxLeft]
+                del list[contMaxLeft]
+                position = search(list, listMinRight[0], positionPivote)
+                deleteByPosition(list, position, listMinRight[0])
+                list.append(MaxLeft)
+                list.insert(0,listMinRight[0])
+                value = listMinRight[0]
+                listMinRight.remove(value)
+                listMinLeft.append(value)
+        contMaxLeft += 1 
                 
-        #Find the minors to the pivot
-        auxCurrentNode = L.head
-        list = []
-        while auxCurrentNode != None:
-            if auxCurrentNode.value < pivot:
-                list.append(auxCurrentNode.value)
-            auxCurrentNode = auxCurrentNode.nextNode
-        #print(list)
+print(listMinLeft)
+print(listMinRight)
+print(list)
 
-        #Add half of the minors to the left
-        halfList = trunc(len(list)/2)
-        for i in range(0,len(list)):
-            if i < halfList:
-                insert(LeftList, list[i], i)
-        
-        #Split the list into two list
-        cont = 0
-        cont2 = 0
-        while currentNode != None:
-            if cont < long:
-                contDuplicates = 0
-                for i in range(0,halfList):
-                    if currentNode.value != list[i]:
-                        contDuplicates += 1
-                if contDuplicates == halfList:
-                    insert(LeftList, currentNode.value, cont)
-            else:
-                insert(RightList, currentNode.value, cont2)
-                cont2 += 1
-            cont += 1
-            currentNode = currentNode.nextNode
-      
-        SubListL = MergeSort(LeftList)
-        SubListR = MergeSort(RightList)
-        currentNodeL = SubListL.head
-        currentNodeR = SubListR.head
-        L.head = None
-        j = 0
-        while currentNodeL != None and currentNodeR != None:
-            if currentNodeL.value < currentNodeR.value:
-                insert(L, currentNodeL.value, j) 
-                currentNodeL = currentNodeL.nextNode
-            else:
-                insert(L, currentNodeR.value, j)
-                currentNodeR = currentNodeR.nextNode
-            j += 1
-        while currentNodeL == None and currentNodeR != None:
-            insert(L, currentNodeR.value, j)
-            j += 1
-            currentNodeR = currentNodeR.nextNode
-        while currentNodeR == None and currentNodeL != None:
-            insert(L, currentNodeL.value, j)
-            j += 1
-            currentNodeL = currentNodeL.nextNode
-        return L
-    
-ListInOrden = MergeSort(List)
-print("Lista (MergeSort):")
-printList(ListInOrden) 
-
-  
+   
     
     
         
