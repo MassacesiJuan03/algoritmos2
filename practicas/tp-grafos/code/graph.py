4b7c7ea13634146e39ab9ec91b76bcfa1153af28
Juan Ignacio <111523496+MassacesiJuan03@users.noreply.github.com>
2023-05-13 17:46:49 -0300

    Add files via upload

--- practicas/tp-grafos/code/graph.py
+++ practicas/tp-grafos/code/graph.py
@@ -86,11 +86,28 @@ def isConnected(Graph):
             return True
         else:
             return False   
-
+#Grafo ponderado conexo.
+def isConnectedWeighted(Graph):
+    #Verificar que el grafo no esté vecío.
+    if Graph == []:
+        return "El grafo se encuentra vacío."
+    else:
+        #Si el grafo no esta vacío entonces calcular su DFS.
+        lista = []
+        lista.append(list(Graph[0].keys()))
+        GraphDFS = convertWeightedToDFSTree(Graph,lista[0][0])
+        if len(GraphDFS) == len(Graph[0]):
+            return True
+        else:
+            return False   
+        
 #Ejercicio 4
-class nodeVertex:
+class nodeVertexDFS:
     value = None
     parent = None
+    color = None
+    distance = 0
+    time = 0
         
 def isTree(Graph):
     #Verificar que el grafo no esté vacío.
@@ -143,7 +160,7 @@ def isTree(Graph):
     return True              
 
 def insertVertex(v,ListNode):
-    vertexNode = nodeVertex()
+    vertexNode = nodeVertexDFS()
     vertexNode.value = v
     ListNode.append(vertexNode)
     return ListNode
@@ -229,6 +246,68 @@ def convertTree(Graph):
                         deleteEdges.append((u,i))
     return deleteEdges
 
+#Ejercicio 7 
+def countConnections(Graph):
+    #Verificar que el grafo no esté vacío.
+    if Graph == []:
+        return "El grafo está vacío"
+    #Obtener los vértices del grafo.
+    listVertex = []
+    for vertex in Graph:
+        listVertex.append(list(vertex.keys()))
+    #Definir los vértices del grafo como nodos con sus atributos.
+    Lnodes = []
+    for vertex in listVertex:
+        listNodes = insertVertex(vertex[0], Lnodes)
+    #Asignar el color a los nodos.
+    for node in listNodes:
+        node.color = "white"
+        node.parent = None
+    #Verificar la cantidad de componentes conexas.
+    time = 0 
+    count = 0
+    listDFS = []
+    for node in listNodes:
+        if node.color == "white":
+            listDFSResult = countConnectionsRecursive(Graph,node,time,listNodes,listDFS)
+            count += 1  
+    return listDFSResult, count
+
+def countConnectionsRecursive(Graph,node,time,listNodes,listDFS):
+    if node.distance == 0:
+        time += 1
+        node.distance = time
+        node.color = "gray"
+    #Obtener la lista de adyacencia de cada vértice.
+    for vertex in Graph:
+        if node.value in vertex:
+            adjacencyNodes = vertex[node.value]
+    #Verificar que la lista de adyacencia de cada nodo no esté vacía.
+    if adjacencyNodes != None:
+        #Buscar los vértices adyacentes de tipo nodo.
+        nodeList = []
+        for vertex in adjacencyNodes:
+            for nodeVertex in listNodes:
+                if nodeVertex.value == vertex:
+                    nodeList.append(nodeVertex)
+        #Recorrer la lista de nodos y realizar el DFS.
+        for nodeVertex in nodeList:
+            if nodeVertex.color == "white":
+                nodeVertex.parent = node
+                #Agregar nodos a la lista final del DFS.
+                dictionary = {}
+                dictionary[node.value] = nodeVertex 
+                listDFS.append(dictionary)
+                countConnectionsRecursive(Graph,nodeVertex,time,listNodes,listDFS)
+    node.color = "black"
+    time += 1
+    node.time = time
+    #Caso base (fin de la recursión)
+    if node.parent == None:
+        return listDFS
+    else:
+        countConnectionsRecursive(Graph,node.parent,time,listNodes,listDFS)               
+    
 #Ejercicio 8
 def convertToBFSTree(Graph, v):
     #Verificar que el grafo no este vacío.
@@ -365,3 +444,362 @@ def convertToDFSTreeRecursive(Graph,v,listVisited,Queue,adjacencyList):
         #No agregar diccionarios vacíos a la lista del DFS.
         if dictionary != {}:
             adjacencyList.append(dictionary)
+
+#Ejercicio 10
+def bestRoad(Graph,v1,v2):
+    #Verificar que el grafo no esté vacío.
+    if Graph == []:
+        return "El grafo está vacío"
+    #Verificar que los vértices v1 y v2 existen dentro del grafo.
+    lista = []
+    for i in range(len(Graph)):
+        lista.append(list(Graph[i].keys()))
+    if [v1] not in lista and [v2] not in lista:
+        return f"El vértice {v1} y el vértice {v2} no se encuentran en el grafo."
+    elif [v1] not in lista:
+        return f"El vértice {v1} no se encuentra en el grafo."
+    elif [v2] not in lista:
+        return f"El vértice {v2} no se encuentra en el grafo."
+    #Definir los nodos a utilizar.
+    ListVertex = []
+    for i in lista:
+        listNodeVertex = insertVertex(i[0],ListVertex)
+    #Definir el color de cada nodo.
+    for node in listNodeVertex:
+        node.color = "white"
+        if node.value == v1:
+            node.color == "gray"
+    #Definir una cola.
+    Queue = []
+    Queue.append(v1)
+    #Definir la lista de salida.
+    listMinPath = []
+    v2NodeVertex = nodeVertex() 
+    while Queue != []:
+        vertex = Queue.pop(0)
+        for node in listNodeVertex:
+                if node.value == vertex:
+                    nodeParent = node
+                    node.color = "black"
+        #Obtener los vértices adyacentes de vertex.
+        adjacencyList = []
+        for dicc in Graph:
+            if vertex in dicc:
+                adjacencyList.append(dicc[vertex])
+        #Verificar que el vértice tenga vértices adyacentes o sea conexo.
+        if adjacencyList != []:
+            for vertexADJ in listNodeVertex:
+                if vertexADJ.value in adjacencyList[0]:
+                    if vertexADJ.color == "white":
+                        vertexADJ.color = "gray"
+                        vertexADJ.parent = nodeParent
+                        vertexADJ.distance = vertexADJ.parent.distance + 1
+                        Queue.append(vertexADJ.value)
+                        if vertexADJ.value == v2:
+                            v2NodeVertex = vertexADJ
+            #Agregar a la lista de salida el camino de v1 a v2.
+            if v2NodeVertex.value == v2:
+                while v2NodeVertex.distance != 0:
+                    listMinPath.insert(0,v2NodeVertex)
+                    v2NodeVertex = v2NodeVertex.parent
+                listMinPath.insert(0,v2NodeVertex)
+                return listMinPath
+    return []                                
+
+#Crear grafo ponderado.
+def createGraphWeighted(listV,listA):
+    #Verificar que la lista de vértices no esté vacía.
+    if listV == []:
+        return "Incorrecto, el grafo no contiene vértices."
+    else:
+        #Definir la lista de adyacencia del grafo.
+        listAdjacency = []
+        #Definir el diccionario que irá dentro de la lista de adyacencia del grafo.  
+        dictionary = {}
+        #Recorrer los vértices del grafo.
+        for i in listV:
+            #Definir la lista de adyacencia de cada vértice del grafo.
+            listAdjacencyInDictionary = []
+            #Recorrer la lista de aristas del grafo.
+            if listA != []:
+                for j in listA:
+                    #Validar el caso de ingresar aristas repetidas(con vértices ordenados o no).
+                    if i == j[0]:
+                        listAdjacencyInDictionary.append((j[1],j[2]))
+                        dictionary[i] = listAdjacencyInDictionary
+                    elif i == j[1]:
+                        listAdjacencyInDictionary.append((j[0],j[2]))
+                        dictionary[i] = listAdjacencyInDictionary
+            #Si el diccionario o la listA estan vacíos es porque el vértice no tiene vértices adyacentes.
+            if listAdjacencyInDictionary == []:
+                dictionary[i] = None
+    listAdjacency.append(dictionary)
+    return listAdjacency
+#DFS para grafo ponderado.
+def convertWeightedToDFSTree(Graph, v):
+    #Verificar que el grafo no este vacio.
+    if Graph == []:
+        return "El Grafo se encuentra vacio."
+    #Verificar que el vértice raíz se encuentre en el grafo.        
+    if v not in Graph[0]:
+        return "El Vértice raíz no se encuentra en el grafo." 
+    #Crear una lista para los vértices visitados.
+    listVisited = []
+    listVisited.append(v)
+    #Crear una cola para colocar los vértices adyacentes no visitados.
+    Queue = []
+    Queue.append(v)
+    #Definir la lista de adyacencia del DFS.
+    adjacencyList = []
+    adjacencyListResult = convertWeightedToDFSTreeRecursive(Graph,v,listVisited,Queue,adjacencyList)
+    return adjacencyListResult
+
+def convertWeightedToDFSTreeRecursive(Graph,v,listVisited,Queue,adjacencyList):
+    #Caso base de la recursividad.
+    if Queue == []:
+        return adjacencyList
+    while Queue != []:
+        #Definir el diccionario que irá en la lista de adyacencia del DFS.
+        dictionary = {}
+        #Lista de adyacencia que va dentro del diccionario.
+        adjacencyListInDictionary = [] 
+        #Si el grafo tiene un solo vértice.
+        if len(Graph[0]) == 1:
+            dictionary[v] = adjacencyListInDictionary
+            adjacencyList.append(dictionary)
+            return adjacencyList
+        #Verificar que el vértice se encuentre en el grafo.
+        if v in Graph[0]:
+            adjacencyNodes = Graph[0][v]
+        else:
+            adjacencyNodes = None
+        #Caso General de la recursividad.
+        if adjacencyNodes != None:
+            count = 0
+            for j in adjacencyNodes:
+                if j[0] in listVisited:
+                    count += 1
+            if count is len(adjacencyNodes):
+                Queue.pop(len(Queue)-1)
+                count = 0
+                #Ingersar los vértices donde se producen ciclos. (linea 131-136)
+                for i in adjacencyList:
+                    if v not in i:
+                        count += 1
+                if count == len(adjacencyList):
+                    dictionary[v] = adjacencyListInDictionary
+                    adjacencyList.append(dictionary)
+                if len(Queue) != 0:
+                    comeBackQueue = convertWeightedToDFSTreeRecursive(Graph,Queue[len(Queue)-1],listVisited,Queue,adjacencyList)
+                else:
+                    comeBackQueue = convertWeightedToDFSTreeRecursive(Graph,Queue,listVisited,Queue,adjacencyList)
+                return comeBackQueue
+        else:
+            return "Vértice no conexo"    
+        #Verificar que el vértice u sea conexo.
+        if adjacencyNodes is not None:
+            #Recorrer la lista de adyacencia del vértice anterior(u).
+            for vertex in adjacencyNodes:
+            #Verificar que el vértice adyacente de u no se encuentre en la lista de visitados.
+                if vertex[0] not in listVisited:
+                    Queue.append(vertex[0])
+                    listVisited.append(vertex[0])
+                    adjacencyListInDictionary.append(vertex[0])
+                    dictionary[v] = adjacencyListInDictionary
+                    v = vertex[0]
+                    break
+        #No agregar diccionarios vacíos a la lista del DFS.
+        if dictionary != {}:
+            adjacencyList.append(dictionary)
+
+#Ejercicio 14
+def PRIM(Graph):
+    #Verificar que el grafo no esté vacío.
+    if Graph == []:
+        return "El grafo está vacío."
+    #Verificar que el grafo sea conexo.
+    connectedGraph = isConnectedWeighted(Graph)
+    if connectedGraph == False:
+        return "El grafo no es conexo."
+    #Obtener los vértices del grafo.
+    listaVertex = []
+    listaVertex.append(list(Graph[0].keys()))
+    #Crear una lista para los vértices visitados.
+    listVisited = []
+    listVisited.append(listaVertex[0][0])
+    #Crear lista de recurrencia para saber los caminos disponibles.
+    listPath = []
+    listPath.append(listaVertex[0][0])
+    #Definir el árbol abarcador de costo mínimo.
+    treeMinimum = []
+    while len(listVisited) != len(Graph[0]):
+        #Definir el diccionario que irá en la lista de adyacencia.
+        dictionary = {}
+        #Si el grafo tiene un solo vértice.
+        if len(Graph[0]) == 1:
+            dictionary[v] = None
+            treeMinimum.append(dictionary)
+            return treeMinimum
+        #Recorrer una lista para encontrar el camino más corto.
+        minWeightPrevius = 99999999999
+        deleteVertex = None
+        for v in listPath:
+            count = 0
+            #Obtener la lista de adyacencia del grafo.
+            if v in Graph[0]:
+                adjacencyNodes = Graph[0][v]
+            #Obtener el menor peso.
+            for weight in adjacencyNodes:
+                if weight[0] not in listVisited:
+                    if weight[1] <= minWeightPrevius:
+                        minWeight = weight[1]
+                        minWeightPrevius = minWeight
+                else:
+                    count += 1
+            #Eliminar vértice de la lista ya que sus aristas no se tendran más en cuenta.
+            if count == len(adjacencyNodes):
+                deleteVertex = v
+            else:
+                #Recorrer la lista de adyacencia del vértice anterior(u).
+                for vertex in adjacencyNodes:
+                    #Verificar que el vértice adyacente de u no se encuentre en la lista de visitados.
+                    if vertex[0] not in listVisited:
+                        if vertex[1] <= minWeight:
+                            u = vertex[0]
+                            key = v
+                            minWeight = vertex[1]
+        listVisited.append(u)
+        listPath.append(u)
+        if deleteVertex != None:
+            listPath.remove(deleteVertex)
+        dictionary[key] = u
+        treeMinimum.append(dictionary) 
+    return treeMinimum
+ 
+#Ejercicio 21
+def createGraphDijkstra(listV, listE):
+    #Verificar que la lista de vértices no esté vacía.
+    if listV == []:
+        return "Incorrecto, el grafo no contiene vértices."
+    else:
+        #Definir la lista de adyacencia del grafo.
+        listAdjacency = []
+        #Definir el diccionario que irá dentro de la lista de adyacencia del grafo.  
+        dictionary = {}
+        #Recorrer los vértices del grafo.
+        for i in listV:
+            #Definir la lista de adyacencia de cada vértice del grafo.
+            listAdjacencyInDictionary = []
+            #Recorrer la lista de aristas del grafo.
+            if listE != []:
+                for j in listE:
+                    #Validar el caso de ingresar aristas repetidas(con vértices ordenados o no).
+                    if i == j[0]:
+                        listAdjacencyInDictionary.append((j[1],j[2]))
+                        dictionary[i] = listAdjacencyInDictionary
+            #Si el diccionario o la listA estan vacíos es porque el vértice no tiene vértices adyacentes.
+            if listAdjacencyInDictionary == []:
+                dictionary[i] = None
+    listAdjacency.append(dictionary)
+    return listAdjacency
+
+class nodeVertex:
+    key = None
+    value = None
+    parent = None
+    distance = 0
+    
+def shortestPath(Graph, s, v):  #Dijkstra
+    if Graph == []:
+        return "El grafo está vacío"
+    #Verificar que los vértices v1 y v2 existen dentro del grafo.
+    lista = []
+    lista.append(list(Graph[0].keys()))
+    if s not in lista[0] and v not in lista[0]:
+        return f"El vértice {s} y el vértice {v} no se encuentran en el grafo."
+    elif s not in lista[0]:
+        return f"El vértice {s} no se encuentra en el grafo."
+    elif v not in lista[0]:
+        return f"El vértice {v} no se encuentra en el grafo."
+    #Crear la lista de nodos.
+    listNodes = []
+    for i in range(len(lista[0])):
+        listResultNodes = insertVertexDijkstra(lista[0][i], listNodes, i)
+    #Init relax
+    for vertexNode in listResultNodes:
+        if vertexNode.value == s:
+            initRelax(listResultNodes, vertexNode)
+            break
+    #Definir lista de nodos visitados.
+    listVisited = []
+    #Definir la cola.
+    Queue = minQueue(listResultNodes)
+    while Queue != []:
+        #Obtener el vértice de la cola.
+        u = Queue.pop(0)
+        #Agregar el vértice a la lista de visitados.
+        listVisited.append(u.value)
+        #Obtener el camino de s a v.
+        if u.value == v:
+            #Si la distancia es la máxima es porque no existe un camino de s a v.
+            if u.distance != 999999999:
+                #Obtener el camino más corto de s a v.
+                listShortestPath = []
+                while u.parent != None:
+                    listShortestPath.insert(0, u)
+                    u = u.parent
+                listShortestPath.insert(0, u)
+                return listShortestPath
+            else:
+                return
+        #Obtener los vértices adyacentes de u.
+        adjacencyNodes = Graph[0][u.value]
+        #Recorrer la lista de adyacencia de u.
+        if adjacencyNodes != None:
+            for vertex in adjacencyNodes:
+                if vertex[0] not in listVisited:
+                    relax(u,vertex, listResultNodes)
+        else:
+            return
+        #Ordenar cola según la distancia en order ascendente
+        minQueue(listResultNodes)      
+    return
+
+def insertVertexDijkstra(v,ListNode,index):
+    #Definir cada vértice del grafo como un nodo con sus atributos.
+    vertexNode = nodeVertex()
+    vertexNode.value = v
+    vertexNode.key = index
+    ListNode.append(vertexNode)
+    return ListNode
+
+def relax(u, tupleV, listNodes):
+    #Obtener el vértice adyacente de u en forma de nodo 
+    for node in listNodes:
+        if node.value == tupleV[0]:
+            v = node
+            break
+    #Realizar relajo
+    if v.distance > (u.distance + tupleV[1]):
+        v.distance = u.distance + tupleV[1]
+        v.parent = u    
+    return
+
+def initRelax(listNodes, v1):
+    #Iniciar el relajamiento para cada vértice(nodo) del grafo.
+    for node in listNodes:
+        node.distance = 999999999
+        node.parent = None
+    v1.distance = 0
+    return
+
+def minQueue(listNodes):
+    count = 0
+    #Ordenar la lista de nodos en orden ascendente según su distancia (usando BubbleSort).
+    while count < len(listNodes) - 1:
+        for i in range(len(listNodes)-1):
+            if listNodes[i].distance > listNodes[i+1].distance:
+                deletedNode = listNodes.pop(i)
+                listNodes.insert(i+1,deletedNode)
+        count += 1
+    return listNodes
--- practicas/tp-grafos/code/main.py
+++ practicas/tp-grafos/code/main.py
@@ -1,4 +1,4 @@
-from graph import createGraph, convertToBFSTree, convertToDFSTree, existPath, isConnected, isTree, isComplete, convertTree
+from graph import createGraph, convertToBFSTree, convertToDFSTree, existPath, isConnected, isTree, isComplete, convertTree, bestRoad, createGraphWeighted, isConnectedWeighted, PRIM, createGraphDijkstra, shortestPath, countConnections
 #Ejercicio 1
 """Implementar la función crear grafo que dada una lista de vértices y una lista de aristas cree un grafo
 con la representación por Lista de Adyacencia.
@@ -10,7 +10,7 @@ de aristas donde por cada par de elementos representa una conexión
 entre dos vértices.
 Salida: retorna el nuevo grafo"""
 listVertex = [1,2,3,4,5,6]
-listEdges = [(1,2),(2,5),(5,3),(3,4),(4,2),(1,5)]
+listEdges = [(1,2),(2,5),(5,3),(3,4),(4,2),(1,5),(3,6)]
 """for i in range(len(listVertex)-1):
     for j in range(i+1,len(listVertex)):
         listEdges.append((listVertex[i],listVertex[j]))"""    
@@ -77,9 +77,27 @@ Descripción: Implementa la operación es convertir a árbol
 Entrada: Grafo con la representación de Lista de Adyacencia.
 Salida: LinkedList de las aristas que se pueden eliminar y el grafo
 resultante se convierte en un árbol."""        
-deleteEdgesList = convertTree(graph)
-print(deleteEdgesList)    
-    
+#deleteEdgesList = convertTree(graph)
+#print(deleteEdgesList)    
+
+#Ejercicio 7
+"""Implementar la función que responde a la siguiente especificación.
+
+def countConnections(Grafo):
+Descripción: Implementa la operación cantidad de componentes conexas
+Entrada: Grafo con la representación de Lista de Adyacencia.
+Salida: retorna el número de componentes conexas que componen el
+grafo."""
+"""DFS, numberOfComponents = countConnections(graph)
+print("DFS:")
+listValue = []
+for dicc in DFS:
+    listValue.append(list(dicc.values()))
+print(listValue)
+for value in listValue:
+    print(value[0].value)
+print(DFS)
+print(numberOfComponents)"""
 
 #Ejercicio 8
 """Implementar la función que responde a la siguiente especificación.
@@ -122,4 +140,114 @@ for i in listVertex:
     print("Lista de adyacencia del DFS, vértice raíz:",i)
     print(GraphDFS)"""    
                 
+#Ejercicio 10
+"""Implementar la función que responde a la siguiente especificación.
+
+def bestRoad(Grafo, v1, v2):
+Descripción: Encuentra el camino más corto, en caso de existir, entre
+dos vértices.
+Entrada: Grafo con la representación de Lista de Adyacencia, v1 y v2
+vértices del grafo.
+Salida: retorna la lista de vértices que representan el camino más
+corto entre v1 y v2. La lista resultante contiene al inicio a v1 y al
+final a v2. En caso que no exista camino se retorna la lista vacía."""    
+
+"""outputList = bestRoad(graph,3,1)
+print(outputList)
+for i in outputList:
+    print("Path:",i.value)"""
+
+#Ejercicio 14    
+"""Implementar la función que responde a la siguiente especificación.
+
+def PRIM(Grafo):
+Descripción: Implementa el algoritmo de PRIM
+Entrada: Grafo con la representación de Matriz de Adyacencia.
+Salida: retorna el árbol abarcador de costo mínimo"""
+V = [1,2,3,4,5]
+E = [(1,2,3),(1,5,1),(2,5,1),(2,4,4),(5,3,2),(4,3,1),(3,2,3)]
+#graphWeighted = createGraphWeighted(V,E)
+#print(graphWeighted)
+#AACM = PRIM(graphWeighted)    
+#print(AACM)   
+#Ejercicio 15
+"""Implementar la función que responde a la siguiente especificación.
+
+def KRUSKAL(Grafo):
+Descripción: Implementa el algoritmo de KRUSKAL
+Entrada: Grafo con la representación de Matriz de Adyacencia.
+Salida: retorna el árbol abarcador de costo mínimo"""
+
+"""def KRUSKAL(Graph):
+    #Verificar que el grafo no esté vacío.
+    if Graph == []:
+        return "El grafo está vacío."
+    #Verificar que el grafo sea conexo.
+    connectedGraph = isConnectedWeighted(Graph)
+    if connectedGraph == False:
+        return "El grafo no es conexo."
+    #Obtener los vértices del grafo.
+    listVertex = []
+    listVertex.append(list(Graph[0].keys()))
+    #Crear las componentes conexas.
+    listComponentConnected = []
+    for vertex in listVertex[0]:
+        LCC = make_set(vertex,listComponentConnected)
+    #Obtener las aristas.
+    listEdges = []
+    for vertex in LCC:
+        for edge in Graph[0][vertex]:
+            edgeTypeList = list(edge)
+            edgeTypeList.insert(0,vertex)
+            listEdges.append(tuple(edgeTypeList))
+    #Eliminar aristas repetidas
+    listPositionDelete = []      
+    for tupleMain in range(len(listEdges)-1):
+        for tupleSecond in range(tupleMain+1,len(listEdges)):
+            if listEdges[tupleMain][0] == listEdges[tupleSecond][1] and listEdges[tupleMain][1] == listEdges[tupleSecond][0]:
+                listPositionDelete.append(listEdges[tupleSecond])
+    for tupleMain in listPositionDelete:
+        listEdges.remove(tupleMain)
+    print(listEdges)
+    #Ordenar las aristas de menor a mayor peso.
+    SortEdgesWeight = sort_by_weight_asc(listEdges)
+    print(SortEdgesWeight)
     
+
+def make_set(vertex, LCC):
+    LCC.append(vertex)
+    return LCC
+
+def sort_by_weight_asc(listEdges): #BubbleSort
+    count = 0
+    while count < len(listEdges) - 1:
+        for i in range(len(listEdges)-1):
+            if listEdges[i][2] > listEdges[i+1][2]:
+                deletedEdge = listEdges.pop(i)
+                listEdges.insert(i+1,deletedEdge)
+        count += 1
+    return listEdges"""    
+
+#AACM = KRUSKAL(graphWeighted)
+#print(E)
+#print(AACM)
+
+#Ejercicio 21
+"""Implementar el Algoritmo de Dijkstra que responde a la siguiente especificación
+
+def shortestPath(Grafo, s, v):
+Descripción: Implementa el algoritmo de Dijkstra
+Entrada: Grafo con la representación de Matriz de Adyacencia, vértice
+de inicio s y destino v.
+Salida: retorna la lista de los vértices que conforman el camino
+iniciando por s y terminando en v. Devolver NONE en caso que no exista
+camino entre s y v."""
+
+"""print("Dijkstra:")
+graphDirected = createGraphDijkstra(V, E)
+print(graphDirected)
+print("Shortest Path:")
+menorCamino = shortestPath(graphDirected, 2, 1)
+print(menorCamino)"""
+#for node in menorCamino:
+    #print(node.value, node.distance)
